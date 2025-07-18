064188e9989de008ea834b9b5f783cb38df81cf8
vishal harkal <168349626+vishalharkal15@users.noreply.github.com>
2025-07-18 17:19:06 +0000

    added requirement.txt

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
 from re import DEBUG
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs. 10,00,000'
+  }, 
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Delhi, India',
+    'salary': 'Rs. 15,00,000'
+  }
+]
+
 @app.route('/')
 def hello_world():
-  return "vishal harkal"
+  return render_template('home.html', jobs=JOBS, company_name='vishal')
 
 
 # print(__name__)
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5001, debug=True)
+  app.run(host='0.0.0.0', port=5000, debug=True)
